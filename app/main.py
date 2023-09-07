435c0de3098dbb48b0c57ac8e56e29b90c778fda
Kerim Mandzo <kerim.mandzo@klika.ba>
2023-09-07 21:16:38 +0200

    pass read_blob tests

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 import sys
 import os
+import zlib
 
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    # print("Logs from your program will appear here!")
 
     # Uncomment this block to pass the first stage
     command = sys.argv[1]
@@ -15,6 +16,16 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/master\n")
         print("Initialized git directory")
+    elif command.startswith("cat-file"):
+        hash = sys.argv[3]
+        directory, file = hash[:2], hash[2:]
+        with open(f".git/objects/{directory}/{file}", "rb") as f:
+            decompressed = zlib.decompress(f.read())
+            x = decompressed.find(b" ")
+            # fmt = decompressed[:x]
+            y = decompressed.find(b"\x00", x)
+            # size = int(decompressed[x:y].decode("ascii"))
+            print(decompressed[y + 1 :].decode("ascii"), end="")
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
